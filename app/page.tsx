1f7f83e80711624e98c5335de11dd678a5f31097
tanvinadkarni13 <159175942+tanvinadkarni13@users.noreply.github.com>
2024-02-06 15:41:32 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
   
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <Link href="/2023/6"> 2023/6 </Link>
+        <Link href="/2023/6" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> 2023/6 </Link>
+        <Link href="/2023/3" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> 2023/3 </Link>
+        <Link href="/2023/1" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> 2023/1 </Link>
       </div>
     </main>
   );
