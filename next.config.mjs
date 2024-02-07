d688babc2f3f4d7dbd6f03c6d0f076806ea1d0ca
tanvinadkarni13 <159175942+tanvinadkarni13@users.noreply.github.com>
2024-02-06 17:23:29 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     compress: false,
+    optimizeFonts: false,
+    webpack: (config, { dev, isServer }) => {
+        config.optimization.minimize = false;
+        return config;
+    },
 };
 
 export default nextConfig;
