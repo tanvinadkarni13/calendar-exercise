16e0d2102902ca1c38868f2108d319101c033280
tanvinadkarni13 <159175942+tanvinadkarni13@users.noreply.github.com>
2024-02-06 17:36:52 -0800

    Update page.tsx

--- app/[year]/[month]/page.tsx
+++ app/[year]/[month]/page.tsx
@@ -2,7 +2,8 @@
 import { useState } from "react";
 import { redirect } from 'next/navigation'
 
-const APP_URL = "https://calendar-exercise-git-main-tanvinadkarni13s-projects.vercel.app/data/events.json"
+// TODO: Should become from some common library/environment and not be hardcoded.
+const APP_URL = "https://calendar-exercise-git-main-tanvinadkarni13s-projects.vercel.app/data/events.json";
 
 type Event = {
     launchDate: string,
@@ -13,20 +14,12 @@ type Event = {
     summary: string,
     learnMoreLink: string,
     purchaseLink: string
-}
+}; 
 
 type CalendarParams = {
     year: string,
     month: string
-}
-
-function isValidYear(year: string) {
-    return year.length === 4 && parseInt(year, 10) > 0
-}
-
-function isValidMonth(month: string) {
-    return parseInt(month, 10) > 0 && parseInt(month, 10) < 13
-}
+};
 
 export default async function Calendar({ params }: { params: CalendarParams }) {
     const { year, month } = params;
@@ -47,7 +40,6 @@ export default async function Calendar({ params }: { params: CalendarParams }) {
         })
     const currentMonthEvents = updatedEvents.filter((item: Event) => item.launchDateObj?.getFullYear() === parseInt(year, 10) && (item.launchDateObj?.getMonth() + 1) === parseInt(month, 10))
     return (
-        // TODO(): Responsiveness needs to be tested. 
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="flex w-full ">
                 <div className="flex-none w-14 h-10">
@@ -96,7 +88,7 @@ function Week({ week, firstDay, days, events }: { week: number, firstDay: number
             setSelectedEvent(event);
         }
     }
-    return (<>
+    return (<div>
         <div className="flex justify-between">
             {daysArray.map((day) => {
                 const dayNumber = week * 7 + day - firstDay + 1;
@@ -107,19 +99,19 @@ function Week({ week, firstDay, days, events }: { week: number, firstDay: number
             })}
         </div>
         {selectedEvent && <EventDetails event={selectedEvent} />}
-    </>
+    </div>
     );
 }
 
 function EventDetails({ event }: { event: Event }) {
     return (
         <div className="w-full px-1 flex items-center justify-center relative ">
-            <img src={`/assets/${event.imageFilenameFull}.webp`} className="h-full" />
-            <section className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-1 text-white  bg-dark bg-opacity-60">
+            <img alt={event.title} src={`/assets/${event.imageFilenameFull}.webp`} className="h-full" />
+            <section className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-1 text-white  bg-dark bg-opacity-60 p2">
                 <h1 className="text-2xl font-extrabold dark:text-white my-1 bg-black bg-opacity-60">{event.title}</h1>
                 {/* event.summary has unsafe html characters and hence not displaying them as markup. */}
-                <p className="my-1 text-sm bg-black bg-opacity-60">{event.summary}</p>
-                <p className="font-extrabold my-1 mb-3 bg-black bg-opacity-60"> <span>Available </span>{event.launchDateObj?.toLocaleString('default', { month: 'long' })}, {event.launchDateObj?.getDate()} {event.launchDateObj?.getFullYear()}</p>
+                <p className="my-1 text-sm bg-black bg-opacity-60 p-2 text-start">{event.summary}</p>
+                <p className="font-extrabold my-1 mb-3 bg-black bg-opacity-60 p-2"> <span>Available </span>{event.launchDateObj?.toLocaleString('default', { month: 'long' })}, {event.launchDateObj?.getDate()} {event.launchDateObj?.getFullYear()}</p>
                 <a href={event.learnMoreLink} target="_new" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">Learn More</a>
                 <a href={event.purchaseLink} target="_new" className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Pre-Order Now</a>
             </section>
@@ -130,12 +122,20 @@ function Day({ day, events, onClick }: { day: number, events: Event[], onClick: 
     const dayEvents = events.filter((event: Event) => event.launchDateObj?.getDate() === day)
     const event = dayEvents.length > 0 ? dayEvents[0] : null;
     if (!event) return (<div className="w-1/6 aspect-square border relative m-1 light:bg-white dark:bg-black shadow"><div className="absolute top-0 right-0 m-1 p-1">{day}</div></div>);
-    return (<div className="w-1/6 aspect-square border relative m-1 bg-white shadow" onClick={() => onClick(event)}>
-        <img src={`/assets/${event.imageFilenameThumb}.webp`} className="w-full h-full" />
+    return (<div className="w-1/6 aspect-square border relative m-1 bg-white shadow cursor-pointer" onClick={() => onClick(event)}>
+        <img alt={event.title} src={`/assets/${event.imageFilenameThumb}.webp`} className="w-full h-full" />
         <div className="absolute top-0 right-0 m-1 bg-sky-500 p-1 rounded-full">{day}</div>
     </div>);
 }
 
 function getDaysInMonth(year: string, month: string) {
     return new Date(parseInt(year, 10), parseInt(month, 10), 0).getDate();
+}
+
+function isValidYear(year: string) {
+    return year.length === 4 && parseInt(year, 10) > 0
+}
+
+function isValidMonth(month: string) {
+    return parseInt(month, 10) > 0 && parseInt(month, 10) < 13
 }
