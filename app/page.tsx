323624fcabc0b0710b8b7e0518b527672989db84
tanvinadkarni13 <159175942+tanvinadkarni13@users.noreply.github.com>
2024-02-06 20:50:30 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from "next/link";
 
 export default function Home() {
@@ -5,9 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
   
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <Link href="/2023/6" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> 2023/6 </Link>
-        <Link href="/2023/3" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> 2023/3 </Link>
-        <Link href="/2023/1" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> 2023/1 </Link>
+        <Link href="/2023/6" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"> Calender Application</Link>
       </div>
     </main>
   );
