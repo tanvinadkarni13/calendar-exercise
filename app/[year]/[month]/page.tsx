7b6f488106408ec178ea98a2e3da2afe3defe440
tanvinadkarni13 <159175942+tanvinadkarni13@users.noreply.github.com>
2024-02-13 00:12:03 -0800

    Update page.tsx

--- app/[year]/[month]/page.tsx
+++ app/[year]/[month]/page.tsx
@@ -33,7 +33,7 @@ function mockAPI(year: number, month: number) {
     const totalList = [
         {
             "id": "7060050195c6b3a514f7300",
-            "launchDate": `${year}-${month}-20T14:36:38.034Z`,
+            "launchDate": `${year}-${month.toString().padStart(2, '0')}-20T14:36:38.034Z`,
             "title": "Fall Guys",
             "summary": "Stumble towards greatness.<br>Fall Guys is a free, cross-platform, massively multiplayer, party royale game where you and your fellow contestants compete through escalating rounds of absurd obstacle course chaos until one lucky victor remains!",
             "imageFilenameThumb": "fall-guys__1x1",
@@ -43,7 +43,7 @@ function mockAPI(year: number, month: number) {
         },
         {
             "id": "2a25599465c6b3a81c11501",
-            "launchDate": `${year}-${month}-11T17:21:14.503Z`,
+            "launchDate": `${year}-${month.toString().padStart(2, '0')}-11T17:21:14.503Z`,
             "title": "Resident Evil 4",
             "summary": "A thrilling reimagining of Capcom's groundbreaking action-horror classic. Resident Evil 4, 2005’s legendary survival horror, is brought fully up-to-date in this ground-up remake.",
             "imageFilenameThumb": "resident-evil-4__1x1",
@@ -53,7 +53,7 @@ function mockAPI(year: number, month: number) {
         },
         {
             "id": "6615829505c6b88b4e30e02",
-            "launchDate": `${year}-${month}-19T20:14:52.687Z`,
+            "launchDate": `${year}-${month.toString().padStart(2, '0')}-19T20:14:52.687Z`,
             "title": "Hogwarts Legacy",
             "summary": "Experience Hogwarts in the late 1800s and decide the fate of the wizarding world.",
             "imageFilenameThumb": "hogwarts-legacy__1x1",
@@ -63,7 +63,7 @@ function mockAPI(year: number, month: number) {
         },
         {
             "id": "18213007415c6b3aceb83602",
-            "launchDate": `${year}-${month}-13T09:01:30.152Z`,
+            "launchDate": `${year}-${month.toString().padStart(2, '0')}-13T09:01:30.152Z`,
             "title": "Gran Turismo 7",
             "summary": "Gran Turismo 7 brings together the very best features of the Real Driving Simulator. Whether you’re a competitive or casual racer, collector, tuner, livery designer or photographer – find your line with a staggering collection of game modes including fan-favorites like GT Campaign, Arcade and Driving School.",
             "imageFilenameThumb": "gran-turismo-7__1x1",
@@ -73,7 +73,7 @@ function mockAPI(year: number, month: number) {
         },
         {
             "id": "939715715c6b3898dd3da5",
-            "launchDate": `${year}-${month}-25T21:32:46.879Z`,
+            "launchDate": `${year}-${month.toString().padStart(2, '0')}-25T21:32:46.879Z`,
             "title": "Metal Gear Solid: The Phantom Pain",
             "summary": "Ushering in a new era for the franchise with cutting-edge technology powered by the Fox Engine, MGSV: The Phantom Pain, will provide players a first-rate gaming experience as they are offered tactical freedom to carry out open-world missions.",
             "imageFilenameThumb": "metal-gear-solid__1x1",
@@ -83,7 +83,7 @@ function mockAPI(year: number, month: number) {
         },
         {
             "id": "1785476915c6b3a21620b48",
-            "launchDate": `${year}-${month}-09T19:45:11.811Z`,
+            "launchDate": `${year}-${month.toString().padStart(2, '0')}-09T19:45:11.811Z`,
             "title": "Megaman 11",
             "summary": "Mega Man is back! The newest entry in this iconic series blends classic, challenging 2D platforming action with a fresh look. The stunning new visual style refreshes the series' iconic colorfulness, combining hand-drawn environments with detailed, 3D character models.",
             "imageFilenameThumb": "megaman-11__1x1",
@@ -93,7 +93,7 @@ function mockAPI(year: number, month: number) {
         },
         {
             "id": "2a25599465c6b3a81c11541",
-            "launchDate": `${year}-${month}-21T15:21:14.503Z`,
+            "launchDate": `${year}-${month.toString().padStart(2, '0')}-21T15:21:14.503Z`,
             "title": "Resident Evil 4",
             "summary": "A thrilling reimagining of Capcom's groundbreaking action-horror classic. Resident Evil 4, 2005’s legendary survival horror, is brought fully up-to-date in this ground-up remake.",
             "imageFilenameThumb": "resident-evil-4__1x1",
@@ -103,7 +103,7 @@ function mockAPI(year: number, month: number) {
         },
         {
             "id": "2143690395c6a39ea9621c0",
-            "launchDate": `${year}-${month}-23T15:21:14.503Z`,
+            "launchDate": `${year}-${month.toString().padStart(2, '0')}-23T15:21:14.503Z`,
             "title": "Hotline Miami",
             "summary": "Hotline Miami is a high-octane action game overflowing with raw brutality, hard-boiled gunplay and skull crushing close combat. Set in an alternative 1989 Miami, you will assume the role of a mysterious antihero on a murderous rampage against the shady underworld at the behest of voices on your answering machine. Soon you'll find yourself struggling to get a grip of what is going on and why you are prone to these acts of violence.",
             "imageFilenameThumb": "hotline-miami__1x1",
@@ -113,7 +113,7 @@ function mockAPI(year: number, month: number) {
         },
         {
             "id": "9692393625c6b390c959715",
-            "launchDate": `${year}-${month}-26T00:29:27.528Z`,
+            "launchDate": `${year}-${month.toString().padStart(2, '0')}-26T00:29:27.528Z`,
             "title": "Spiderman 2",
             "summary": "BE GREATER. TOGETHER. Spider-Men, Peter Parker and Miles Morales, return for an exciting new adventure in the critically acclaimed Marvel’s Spider-Man franchise for PS5. <br> Swing, jump and utilize the new Web Wings to travel across Marvel’s New York, quickly switching between Peter Parker and Miles Morales to experience different stories and epic new powers, as the iconic villain Venom threatens to destroy their lives, their city and the ones they love.",
             "imageFilenameThumb": "spiderman-2__1x1",
@@ -123,7 +123,7 @@ function mockAPI(year: number, month: number) {
         },
         {
             "id": "9692393625c6b390c959715",
-            "launchDate": `${year}-${month}-26T00:29:27.528Z`,
+            "launchDate": `${year}-${month.toString().padStart(2, '0')}-26T00:29:27.528Z`,
             "title": "Spiderman 2",
             "summary": "BE GREATER. TOGETHER. Spider-Men, Peter Parker and Miles Morales, return for an exciting new adventure in the critically acclaimed Marvel’s Spider-Man franchise for PS5. <br> Swing, jump and utilize the new Web Wings to travel across Marvel’s New York, quickly switching between Peter Parker and Miles Morales to experience different stories and epic new powers, as the iconic villain Venom threatens to destroy their lives, their city and the ones they love.",
             "imageFilenameThumb": "spiderman-2__1x1",
@@ -134,17 +134,7 @@ function mockAPI(year: number, month: number) {
     ];
     switch (month) {
         case 1:
-            return [
-                totalList[0],
-                totalList[1],
-                totalList[2],
-            ]
         case 5:
-            return [
-                totalList[0],
-                totalList[1],
-                totalList[2],
-            ]
         case 9:
             return [
                 totalList[0],
@@ -152,17 +142,7 @@ function mockAPI(year: number, month: number) {
                 totalList[2],
             ]
         case 2:
-            return [
-                totalList[3],
-                totalList[4],
-                totalList[5],
-            ]
         case 6:
-            return [
-                totalList[3],
-                totalList[4],
-                totalList[5],
-            ]
         case 10:
             return [
                 totalList[3],
@@ -170,17 +150,7 @@ function mockAPI(year: number, month: number) {
                 totalList[5],
             ]
         case 3:
-            return [
-                totalList[6],
-                totalList[7],
-                totalList[8],
-            ]
         case 7:
-            return [
-                totalList[6],
-                totalList[7],
-                totalList[8],
-            ]
         case 11:
             return [
                 totalList[6],
@@ -188,17 +158,7 @@ function mockAPI(year: number, month: number) {
                 totalList[8],
             ]
         case 4:
-            return [
-                totalList[9],
-                totalList[1],
-                totalList[0],
-            ]
         case 8:
-            return [
-                totalList[9],
-                totalList[1],
-                totalList[0],
-            ]
         case 12:
             return [
                 totalList[9],
@@ -207,9 +167,7 @@ function mockAPI(year: number, month: number) {
             ]
     }
     return [
-        totalList[0],
-        totalList[4],
-        totalList[8],
+       
     ];
 }
 // SIMULATING A REQUEST TO THE API
